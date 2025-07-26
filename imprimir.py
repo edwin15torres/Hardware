97dcfb4922a462155dbcb840aedb38bc87138e36
edwin_torres <ea.torresl1@uniandes.edu.co>
2025-07-26 13:49:30 -0500

    Corrección de tamaño de impresión

--- imprimir.py
+++ imprimir.py
@@ -1,7 +1,7 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QTextEdit
-from PyQt5.QtPrintSupport import QPrinter, QPrintDialog, QPrintPreviewDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
+from PyQt5.QtPrintSupport import QPrinter, QPrintPreviewDialog
 from PyQt5.QtGui import QTextDocument
-
+from PyQt5.QtCore import QSizeF
 import sys
 
 class VentanaFactura(QWidget):
@@ -11,11 +11,8 @@ class VentanaFactura(QWidget):
         self.setFixedSize(600, 400)
 
         layout = QVBoxLayout(self)
-
-        # Botón para mostrar vista previa
-        self.btn_previsualizar = QPushButton("Previsualizar e Imprimir Factura")
+        self.btn_previsualizar = QPushButton("Previsualizar e Imprimir/Guardar como PDF")
         self.btn_previsualizar.clicked.connect(self.mostrar_vista_previa)
-
         layout.addWidget(self.btn_previsualizar)
 
     def generar_html_factura(self):
@@ -23,35 +20,29 @@ class VentanaFactura(QWidget):
         <h2 style='text-align:center;'>Factura de Venta</h2>
         <p><strong>Cliente:</strong> Juan Pérez</p>
         <p><strong>Fecha:</strong> 25/07/2025</p>
-        <table border='1' cellspacing='0' cellpadding='5' width='100%'>
-            <tr>
-                <th>Producto</th><th>Cantidad</th><th>Precio Unitario</th><th>Total</th>
-            </tr>
-            <tr>
-                <td>Pollo Asado</td><td>2</td><td>$15.000</td><td>$30.000</td>
-            </tr>
-            <tr>
-                <td>Cerveza</td><td>3</td><td>$5.000</td><td>$15.000</td>
-            </tr>
-            <tr>
-                <td colspan='3' align='right'><strong>Total a Pagar:</strong></td>
-                <td><strong>$45.000</strong></td>
-            </tr>
+        <table border='1' cellspacing='0' cellpadding='4' width='100%'>
+            <tr><th>Producto</th><th>Cantidad</th><th>Precio</th></tr>
+            <tr><td>Salchicha</td><td>2</td><td>$8.000</td></tr>
+            <tr><td>Gaseosa</td><td>1</td><td>$2.000</td></tr>
         </table>
+        <p style='text-align:right;'><strong>Total:</strong> $10.000</p>
         """
 
     def mostrar_vista_previa(self):
+        documento = QTextDocument()
+        documento.setHtml(self.generar_html_factura())
+
         printer = QPrinter(QPrinter.HighResolution)
-        dialogo_vista_previa = QPrintPreviewDialog(printer, self)
-        dialogo_vista_previa.paintRequested.connect(self.imprimir_factura)
-        dialogo_vista_previa.exec_()
+        printer.setPageSize(QPrinter.Custom)
+        printer.setPaperSize(QSizeF(58, 297), QPrinter.Millimeter)
+        printer.setFullPage(True)
 
-    def imprimir_factura(self, printer):
-        doc = QTextDocument()
-        doc.setHtml(self.generar_html_factura())
-        doc.print_(printer)
+        preview = QPrintPreviewDialog(printer, self)
+        preview.setWindowTitle("Vista previa de la factura")
+        preview.paintRequested.connect(documento.print_)
+        preview.exec_()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     ventana = VentanaFactura()
     ventana.show()
